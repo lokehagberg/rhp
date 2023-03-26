894c287c5388944af4735e6b823bb91307c6874e
Loke Hagberg <loke_hagberg@hotmail.com>
2023-03-26 13:59:21 +0200

    updates according to formalism change

--- rhp_plan.py
+++ rhp_plan.py
@@ -48,55 +48,57 @@ def concatenator(depreciation_list, supply_use_list, planning_horizon, T):
             
     return(DJ_aggregated)
 
-#Plan function
-def plan(time_steps, planning_horizon, primary_resource_list, supply_use_list, use_imported_list, depreciation_list, 
-         domestic_target_output_list, imported_target_output_list, export_constraint, export_vector_list, export_prices_list, import_prices_list):
+#Plan function; full designates including exports
+def plan(time_steps, planning_horizon, primary_resource_list, supply_use_list, use_imported_list, 
+         depreciation_list, full_domestic_target_output_list, imported_target_output_list, 
+         export_constraint_boolean, export_target_list, export_prices_list, import_prices_list):
     
     result_list, lagrange_list, slack_list = [], [], []
 
     for T in range(time_steps):
 
         # Export constraints
-        import_cost_matrix = deepcopy(use_imported_list[T])
-        for i in range(use_imported_list[T].shape[0]):
-            for j in range(use_imported_list[T].shape[1]):
-                import_cost_matrix[i, j] = use_imported_list[T][i, j] * import_prices_list[T][i, 0]
+        # import_cost_matrix = deepcopy(use_imported_list[T])
+        # for i in range(use_imported_list[T].shape[0]):
+        #     for j in range(use_imported_list[T].shape[1]):
+        #         import_cost_matrix[i, j] = use_imported_list[T][i, j] * import_prices_list[T][i, 0]
                 
-        import_cost_list = deepcopy([import_cost_matrix])
+        # import_cost_list = deepcopy([import_cost_matrix])
         
-        for i in range(planning_horizon - 1):
-            cost_list = deepcopy(np.concatenate((import_cost_list[0], import_cost_matrix), axis=1))
-            import_cost_list[0] = deepcopy(cost_list)
+        # for i in range(planning_horizon - 1):
+        #     cost_list = deepcopy(np.concatenate((import_cost_list[0], import_cost_matrix), axis=1))
+        #     import_cost_list[0] = deepcopy(cost_list)
             
-        augmented_import_cost_matrix = import_cost_list[0].sum(axis=0)
-        export_value_list = []
-        for i in range(time_steps):
-            export_value_list.append(np.dot(export_prices_list[i].reshape([1,-1]), export_vector_list[i]))
+        # augmented_import_cost_matrix = import_cost_list[0].sum(axis=0)
+        # export_value_list = []
+        # for i in range(time_steps):
+        #     export_value_list.append(np.dot(export_prices_list[i].reshape([1,-1]), export_vector_list[i]))
 
         # Constructing DJ aggregated
         non_exp_DJ_aggregated = concatenator(depreciation_list, supply_use_list, planning_horizon, T)
         
-        DJ_aggregated = np.vstack((concatenator(depreciation_list, supply_use_list, planning_horizon, T), 
-                                   -augmented_import_cost_matrix))
+        # DJ_aggregated = np.vstack((concatenator(depreciation_list, supply_use_list, planning_horizon, T), 
+        #                            -augmented_import_cost_matrix))
         
         # Constructing Dr aggregated
-        depreciated_target_output_list = [domestic_target_output_list[T]]
+        depreciated_target_output_list = [full_domestic_target_output_list[T]]
         for i in range(planning_horizon - 1):
             depreciated_target_output_list.append(
-                domestic_target_output_list[T + i + 1] + 
+                full_domestic_target_output_list[T + i + 1] + 
                 np.matmul(depreciation_list[T + i + 2], depreciated_target_output_list[i]))
         
         non_exp_Dr_aggregated = stack_vertical(depreciated_target_output_list, planning_horizon - 1, 0)
         
-        Dr_aggregated = np.concatenate((stack_vertical(depreciated_target_output_list, planning_horizon - 1, 0), 
-                                        np.dot(imported_target_output_list[T],import_prices_list[T])-export_value_list[T]))
+        # Dr_aggregated = np.concatenate((stack_vertical(depreciated_target_output_list, planning_horizon - 1, 0), 
+        #                                 np.dot(imported_target_output_list[T],import_prices_list[T])-export_value_list[T]))
 
         # Constructing c aggregated
         c_aggregated = stack_vertical(primary_resource_list, planning_horizon - 1, T)
         
-        if export_constraint:
-            DJ_matrix = deepcopy(DJ_aggregated)
-            Dr_vector = deepcopy(Dr_aggregated)
+        if export_constraint_boolean:
+            # DJ_matrix = deepcopy(DJ_aggregated)
+            # Dr_vector = deepcopy(Dr_aggregated)
+            pass
         else:
             DJ_matrix = deepcopy(non_exp_DJ_aggregated)
             Dr_vector = deepcopy(non_exp_Dr_aggregated)
