7b61909f476bff6df06f48cc667c63c2a1b3efeb
Loke Hagberg <loke_hagberg@hotmail.com>
2023-03-27 19:18:54 +0200

    Examples fixed for non export constraint

--- rhp_plan.py
+++ rhp_plan.py
@@ -32,7 +32,7 @@ def plan(time_steps, planning_horizon, primary_resource_list, supply_use_list, u
         vertical_block_list = []
         modifier = deepcopy(modifier_value)
         for i in range(planning_horizon+1):
-            vertical_block_list.append(np.add(full_domestic_target_output_list[N+i], modifier))
+            vertical_block_list.append(full_domestic_target_output_list[N+i] - modifier)
             #depreciation and carry
             modifier = deepcopy(np.matmul(depreciation_list[N+i], 
                                           np.sum(vertical_block_list, axis=0)))
@@ -100,10 +100,10 @@ def plan(time_steps, planning_horizon, primary_resource_list, supply_use_list, u
 
         #Production carry into the next time step
         if export_constraint_boolean:
-            recent_slack = np.array_split(slack_list[N], planning_horizon+2)
+            recent_slack = deepcopy(np.array_split(slack_list[N], planning_horizon+2))
             modifier_value = deepcopy(np.matmul(depreciation_list[N], recent_slack[0]))
         else:
-            recent_slack = np.array_split(slack_list[N], planning_horizon+1)
+            recent_slack = deepcopy(np.array_split(slack_list[N], planning_horizon+1))
             modifier_value = deepcopy(np.matmul(depreciation_list[N], recent_slack[0]).reshape([-1,1]))
 
     return([result_list, lagrange_list, slack_list, aggregate_primary_resource_vector, aggregate_constraint_matrix, aggregate_constraint_vector])
